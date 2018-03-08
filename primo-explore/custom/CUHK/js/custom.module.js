d750b44307acc7f6ec2c9abbeee59de0e82e28d7
Alex Chan <alexchanwk@gmail.com>
2018-03-08 21:20:19 +0800

    infinite scroll

--- primo-explore/custom/CUHK/js/custom.module.js
+++ primo-explore/custom/CUHK/js/custom.module.js
@@ -1,1 +1,1 @@
-var app = angular.module('viewCustom', ['angularLoad']).constant('IS_PRODUCTION', window.location.hostname == 'julac.hosted.exlibrisgroup.com' ? true : false);
+var app = angular.module('viewCustom', ['angularLoad']).constant('IS_PRODUCTION', window.location.hostname == 'julac.hosted.exlibrisgroup.com' ? true : false).value('nextPageCtrl', { lastY: 0});
---
+++ primo-explore/custom/CUHK/js/primo-explore-prm-explore-main-after.js
@@ -0,0 +1,31 @@
+app.controller('prmExploreMainAfterController', ['$scope', '$timeout', 'IS_PRODUCTION', 'nextPageCtrl', function ($scope, $timeout, IS_PRODUCTION, nextPageCtrl) {
+    // Sandbox Label
+    $scope.IS_PRODUCTION = IS_PRODUCTION;
+    if (!IS_PRODUCTION) {
+        angular.element(document).find('body').addClass("body-margin");
+    }
+
+    // Infinite Scroll
+    angular.element(document).bind('scroll', function () {
+        $scope.lastY = nextPageCtrl.lastY;
+        var lastItem = angular.element(document.querySelector('div.results-container .last-item'))[0];
+        if (!lastItem)
+            return;
+        var lastItemY = lastItem.offsetTop;
+
+        if ($scope.lastY < lastItemY && window.pageYOffset > lastItemY + 300 - window.innerHeight)
+        {
+            $scope.lastY = nextPageCtrl.lastY = lastItemY;
+            angular.element(document.querySelector('div.results-container button.button-confirm:last-child')).triggerHandler('click');
+            console.log(window.pageYOffset + ", " + (lastItemY + 300 - window.innerHeight, 10));
+        }
+    });
+}]);
+
+app.component('prmExploreMainAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmExploreMainAfterController',
+    template: `
+        <div ng-if="!IS_PRODUCTION" class="sandbox-label">Sandbox</div>
+        `
+});
