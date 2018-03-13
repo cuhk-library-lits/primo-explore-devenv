0bf988b4b2346e7ffd94f9d3440446d2d806f508
Alex Chan <alexchanwk@gmail.com>
2018-03-13 15:40:05 +0800

    UI customizations

--- primo-explore/custom/CUHK/js/custom.module.js
+++ primo-explore/custom/CUHK/js/custom.module.js
@@ -1,1 +1,1 @@
-var app = angular.module('viewCustom', ['angularLoad']).constant('IS_PRODUCTION', window.location.hostname == 'julac.hosted.exlibrisgroup.com' ? true : false).value('nextPageCtrl', { lastY: 0});
+var app = angular.module('viewCustom', ['angularLoad']).constant('_', window._).constant('IS_PRODUCTION', window.location.hostname == 'julac.hosted.exlibrisgroup.com' ? true : false).value('nextPageCtrl', { lastY: 0});
---
+++ primo-explore/custom/CUHK/js/primo-explore-viewit-linkto-hkall.js
@@ -0,0 +1,19 @@
+/*
+  Note: Choose "Inherit from Central package" when deploy to use consortium elements
+*/
+
+/*
+The definition for the after directive that is implemnted in the other
+component
+Note:
+<julac-link-to-hkall parent-ctrl="$ctrl.parentCtrl"></julac-link-to-hkall>
+is in development and can be added once completed
+*/
+
+app.component('prmFullViewServiceContainerAfter', {
+    bindings: { parentCtrl: '<' },
+    template: `
+               <julac-link-to-hkall parent-ctrl="$ctrl.parentCtrl"></julac-link-to-hkall>
+               <julac-view-it-from-other-inst parent-ctrl="$ctrl.parentCtrl"></julac-view-it-from-other-inst>
+    `
+});
