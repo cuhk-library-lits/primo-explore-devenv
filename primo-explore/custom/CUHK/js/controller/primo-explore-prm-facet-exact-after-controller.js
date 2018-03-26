4155bb8341e08873ad73ac5454c40e29c534c06c
Alex Chan <alexchanwk@gmail.com>
2018-03-26 21:32:39 +0800

    Facet label

--- primo-explore/custom/CUHK/js/controller/primo-explore-prm-facet-exact-after-controller.js
+++ primo-explore/custom/CUHK/js/controller/primo-explore-prm-facet-exact-after-controller.js
@@ -1,16 +1,23 @@
 app.controller('prmFacetExactAfterController', ['FacetCountLimitService', function (FacetCountLimitService) {
     var ctrl = this;
     var facetLimitCount = FacetCountLimitService.facetLimitCount;
+    var facetLimitLabel = FacetCountLimitService.facetLimitLabel;
 
     ctrl.facetLimitCount = facetLimitCount;
+    ctrl.facetLimitLabel = facetLimitLabel;
+    ctrl.showFacetLimitCount = false;
     
     var facetGroup = ctrl.parentCtrl.facetGroup;
     if (facetGroup && facetGroup.values && facetGroup.values.length > facetLimitCount) {
         facetGroup.values.splice(facetLimitCount, facetGroup.values.length - facetLimitCount);
+        ctrl.showFacetLimitCount = true;
     }
 }]);
 
 app.component('prmFacetExactAfter', {
     bindings: { parentCtrl: '<' },
-    controller: 'prmFacetExactAfterController'
+    controller: 'prmFacetExactAfterController',
+    template: `
+        <span class="facet-limit-label" ng-if="$ctrl.showFacetLimitCount">{{$ctrl.facetLimitLabel}}</span>
+        `
 });
--- primo-explore/custom/CUHK/js/service/primo-explore-facet-count-limit-service.js
+++ primo-explore/custom/CUHK/js/service/primo-explore-facet-count-limit-service.js
@@ -1,7 +1,17 @@
 var FACET_LIMIT_COUNT = 20;
 
-function FacetCountLimitService($document) {
-    this.facetLimitCount = FACET_LIMIT_COUNT
+function FacetCountLimitService($document, $location) {
+    this.facetLimitCount = FACET_LIMIT_COUNT;
+    this.lang = $location.search().lang;
+    this.facetLimitLabel = "* Only top " + FACET_LIMIT_COUNT + " entries are displayed";
+    switch (this.lang) {
+        case "zh_TW":
+            this.facetLimitLabel = "* 只顯示首" + FACET_LIMIT_COUNT + "項";
+            break;
+        case "zh_CN":
+            this.facetLimitLabel = "* 只显示首" + FACET_LIMIT_COUNT + "项";
+            break;
+    }
 }
 
-app.service('FacetCountLimitService', ['$document', FacetCountLimitService]);
+app.service('FacetCountLimitService', ['$document', '$location', FacetCountLimitService]);
