e3ffe2a58921734e8126d19b428432622a2e1862
Alex Chan <alexchanwk@gmail.com>
2018-10-16 13:09:37 +0800

    Remove unused

--- primo-explore/custom/CUHK/js/controller/primo-explore-prm-full-view-service-container-after.js
+++
@@ -1,9 +0,0 @@
-/*
-app.component('prmFullViewServiceContainerAfter', {
-    bindings: { parentCtrl: '<' },
-    template: `
-        <julac-link-to-hkall parent-ctrl="$ctrl.parentCtrl"></julac-link-to-hkall>
-        <julac-view-it-from-other-inst parent-ctrl="$ctrl.parentCtrl"></julac-view-it-from-other-inst>
-    `
-});
-*/
