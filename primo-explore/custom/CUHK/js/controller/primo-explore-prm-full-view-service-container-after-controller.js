e9e3a03c094f297a3f39e7aaea646ba6c4772574
Alex Chan <alexchanwk@gmail.com>
2019-06-21 10:04:58 +0800

    Fulfillment CUHK - 'No HKALL Service' Configuration

---
+++ primo-explore/custom/CUHK/js/controller/primo-explore-prm-full-view-service-container-after-controller.js
@@ -0,0 +1,6 @@
+app.component("prmFullViewServiceContainerAfter", {
+  bindings: { parentCtrl: "<" },
+  template: `
+    <julac-add-hkall-info-to-openurl parent-ctrl="$ctrl.parentCtrl"></julac-add-hkall-info-to-openurl>
+    `
+});
